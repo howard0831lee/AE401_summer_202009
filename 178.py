3f2c3eed1fa17bf63872d61c65997270d4554b84
howard0831lee <73009358+howard0831lee@users.noreply.github.com>
2020-10-31 12:03:02 +0800

    Add files via upload

---
+++ 178.py
@@ -0,0 +1,11 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+x,y,z = mc.player.getTilePos()
+
+try:
+    blockId = int(input('方塊ID?'))
+    mc.setBlock(x,y,z,blockId)
+    
+except:
+    mc.postToChat("數字????????????")
---
+++ 66666666666666666666666666666666.py
@@ -0,0 +1,11 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+x,y,z = mc.player.getTilePos()
+
+length = 50
+width = 50
+height = 50
+
+block = 169
+air = 0
+lamp = 57
