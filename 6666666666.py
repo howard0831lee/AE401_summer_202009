2f28d4f796a9a3979e09891d962a5c324a32627c
howard0831lee <73009358+howard0831lee@users.noreply.github.com>
2020-11-07 12:36:20 +0800

    Add files via upload

---
+++ 6666666666.py
@@ -0,0 +1,12 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+while True:
+    hits = mc.events.pollBlockHits()
+    if len(hits) > 0:
+        hit = hits[0]
+        x,y,z = hit.pos
+        block = mc.getBlock(x,y,z)
+        
+        mc.postToChat('它是:' +
+                      str(block))
